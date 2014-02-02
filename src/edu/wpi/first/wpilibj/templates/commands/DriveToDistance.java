f63df57804ca892cdd36ef3bc4965cb0a3faa05b
Andrew <vgdude999@gmail.com>
2014-02-02 16:55:14 -0500

    experimental distance drive, replacing PID

--- src/edu/wpi/first/wpilibj/templates/commands/DriveToDistance.java
+++ src/edu/wpi/first/wpilibj/templates/commands/DriveToDistance.java
@@ -12,27 +12,49 @@ import edu.wpi.first.wpilibj.templates.Robot;
  * @author Andrew
  */
 public class DriveToDistance extends Command {
-    double setpoint;
+    private double setpoint;
+    private double initDistance;
+    private double speed;
+    private final double SLOW = .25;
+    private final double MED = .5;
+    private final double FAST = .7;
+    private double next;
+    private final double DIST_SCALE = .75;
+    private final double SPEED_SCALE = .75;
     public DriveToDistance(double setpoint) {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
         requires(Robot.drivetrain);
         this.setpoint = setpoint;
+        initDistance = Robot.drivetrain.getDistance()-setpoint;
+        next = initDistance*.75;
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
-        Robot.drivetrain.setSetpoint(setpoint);
+        /*Robot.drivetrain.setSetpoint(setpoint);        
         Robot.drivetrain.enable();
+        */
+        if(initDistance <= 20)
+            speed = SLOW;
+        else if(initDistance < 50)
+            speed = MED;
+        else speed = FAST;
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+        if(Robot.drivetrain.getDistance() < next){
+            next = next*DIST_SCALE;
+            speed = speed*SPEED_SCALE;
+        }
+        
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return Math.abs(Robot.drivetrain.getDistance()-setpoint) < .02;
+        //return Math.abs(Robot.drivetrain.getDistance()-setpoint) < .02;
+        return Robot.drivetrain.getDistance() <=setpoint;
     }
 
     // Called once after isFinished returns true
